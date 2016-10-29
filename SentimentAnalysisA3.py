f1e5e00828ae4211dd6171cd47bbb067c54fab21
Kiley Fischer <kfischer2@babson.edu>
2016-10-29 16:35:45 -0400

    Update SentimentAnalysisA3.py

--- SentimentAnalysisA3.py
+++ SentimentAnalysisA3.py
@@ -1,15 +1,29 @@
 import nltk
-import re
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 text = open('Treasure_Island.txt','r')
-pos_word = open('positive-words.txt', 'r')
-neg_word = open('negative-words.txt', 'r')
-posSentences = re.split(r'\n', pos_word.read())
-negSentences = re.split(r'\n', neg_word.read())
- 
-posFeatures = []
-negFeatures = []
-    #http://stackoverf
+pos = open('positive-words.txt', 'r')
+neg = open('negative-words.txt', 'r')
+
+def process_file(filename, skip_header):
+    hist = {}
+    fp = open(filename)
+    if skip_header:
+        skip_gutenberg_header(fp)
+    for line in fp:
+        if line.startswith('*** END OF THIS PROJECT'):
+            break
+        for word in line.split():
+            word = word.lower()
+            hist[word] = hist.get(word,0) + 1 
+    return hist
+
+def skip_gutenberg_header(fp):
+    """Reads from fp until it finds the line that ends the header.
+    fp: open file object
+    """
+    for line in fp:
+        if line.startswith('*** START OF THIS PROJECT'):
+            break
 
 def tokenize(text):
     # remove non letters
@@ -20,7 +34,16 @@ def tokenize(text):
     stems = stem_tokens(tokens, stemmer)
     return stems
 
-vectorizer = CountVectorizer( analyzer = 'word',tokenizer = tokenize, lowercase = True,stop_words = 'english',
-    max_features = 85)
-score = SentimentIntensityAnalyzer().polarity_scores('text')
-print(score)
+
+def main():
+    print('Sentiment Analysis of "Treasure Island"')
+    hist1 = process_file('Treasure_Island.txt', skip_header = True)
+    tokenizer = tokenize
+    lowercase = True
+    stop_words = 'english'
+    score = SentimentIntensityAnalyzer().polarity_scores('hist1')
+    print(score)
+
+
+if __name__ == '__main__':
+    main()
